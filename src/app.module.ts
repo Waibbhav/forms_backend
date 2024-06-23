05538564eef76228fe7846f639204c093e388ed8
Waibbhav <Singhwaibbhav@gmail.com>
2024-06-23 13:23:58 +0530

    db connection established

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_URL, {}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
